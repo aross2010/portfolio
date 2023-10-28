1b20c01c8603a105e8d3aca1e61ac7dbd47c4fc7
Alex Ross <adross1027@gmail.com>
2023-10-27 17:18:07 -0700

    updated links

--- components/intro.tsx
+++ components/intro.tsx
@@ -110,7 +110,7 @@ export default function Intro() {
           className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full
           focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60 max-h-[3rem] max-w-[3rem]
         "
-          href="https://linkedin.com"
+          href="https://www.linkedin.com/in/alex-ross-32b278236/"
           target="_blank"
         >
           <FaLinkedin />
@@ -118,7 +118,7 @@ export default function Intro() {
 
         <a
           className="bg-white p-4 text-[1.35rem] text-gray-700 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60 max-h-[3rem] max-w-[3rem]"
-          href="https://github.com"
+          href="https://github.com/aross2010"
           target="_blank"
         >
           <FaGithubSquare />
